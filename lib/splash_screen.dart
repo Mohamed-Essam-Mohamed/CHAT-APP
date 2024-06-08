b6a2e590b761751ec67933c9980a5bdfdea91765
mohamed <esammww.com@gmail.com>
2024-06-08 12:59:03 +0300

    finished splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const routeName = 'SplashScreen';
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed(SplashScreen.routeName);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
