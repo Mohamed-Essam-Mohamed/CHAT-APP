422ece0bdcf854e9cc833603515ed2007e32a65f
mohamed <esammww.com@gmail.com>
2024-06-09 09:42:30 +0300

    finished connection firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/splash_screen.dart';
 import 'package:chat_app/src/feature/auth/login/view/login_screen.dart';
 import 'package:chat_app/src/feature/auth/register/view/register_screen.dart';
-import 'package:chat_app/src/utils/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChatApp());
 }
 
